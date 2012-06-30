3d7eba3189e622c92dd26573a5b0fbd66c795d6d
Luis Sala <lsala@primogen.com>
2012-06-30 11:22:16 -0700

    Initial Commit

---
+++ README.txt
@@ -0,0 +1,91 @@
+INSTALLATION
+============
+
+The Web Script SSO AUthenticator is a drop-in replacement for the default Basic Authentication support.
+
+It works by looking for a REMOTE_USER server variable. If present, then that User ID will be used to
+authenticate the request.
+
+0. These instructions assume you're in the `{ALFRESCO_HOME}` directory.
+
+1. To apply the AMP, copy `alfresco-webscript-sso.amp` to `{ALFRESCO_HOME}/amps`.
+
+2. Run `./bin/apply_amps.sh`.
+
+3. After applying the AMP, start Alfresco to force deployment of the updated WAR archive:
+	eg. `alfresco.sh start`
+
+4. Once Alfresco has been started, you must shut it back down again:
+	eg. `alfresco.sh stop`
+	
+5. Edit `{ALFRESCO_HOME}/tomcat/webapps/alfresco/WEB-INF/web.xml` and make the following changes:
+
+	=============
+	Replace this:
+	=============
+	
+	   <servlet>
+	      <servlet-name>apiServlet</servlet-name>
+	      <servlet-class>org.springframework.extensions.webscripts.servlet.WebScriptServlet</servlet-class>
+	      <init-param>
+	         <param-name>authenticator</param-name>
+	         <param-value>webscripts.authenticator.basic</param-value>
+	      </init-param>
+	   </servlet>
+	   
+	==========
+	With this:
+	==========
+	
+	   <servlet>
+	      <servlet-name>apiServlet</servlet-name>
+	      <servlet-class>org.springframework.extensions.webscripts.servlet.WebScriptServlet</servlet-class>
+	      <init-param>
+	         <param-name>authenticator</param-name>
+	         <param-value>webscripts.authenticator.servicesso</param-value>
+	      </init-param>
+	   </servlet>
+	   
+6. After saving `web.xml`, you may launch Alfresco again.
+
+Special Note: For development and testing purposes it is OK for these manual changes to be applied to
+the "unpacked" `web.xml` file. This means that you will need to repeat steps 3-6 if new AMPs are ever
+deployed.
+
+Production installations should incorporate these changes directly into `alfresco.war` by unpacking the
+war file into a temporary directory:
+	eg.	`jar xf alfresco.war -C ~/tmp_war/`
+
+After applying the changes to `WEB-INF/web.xml` you may re-pack the war using:
+	eg.	`jar cvf alfresco.war .`
+	
+	
+USING WITH HTTP HEADERS
+=======================
+
+NOTE: For testing and debugging purposes only
+
+This AMP support the use of a `REMOTE_USER` HTTP header. This feature is disabled by default but may be
+enabled by creating a new context file and overriding the authenticator JavaBean with new properties.
+
+1. Create and edit a new file called:
+	`{ALFRESCO_HOME}/tomcat/shared/classes/alfresco/extension/custom-webscript-sso-context.xml`
+
+2. Paste in the following:	
+	<?xml version='1.0' encoding='UTF-8'?>
+	<!DOCTYPE beans PUBLIC '-//SPRING//DTD BEAN//EN' 'http://www.springframework.org/dtd/spring-beans.dtd'>
+	<beans>
+	
+	  <!-- Custom Web Script authenticator used to provide a custom authentication scheme in front of CMIS -->
+	  <bean id="webscripts.authenticator.servicesso" class="org.alfresco.module.webscripts.sso.SSOAuthenticatorFactory">
+	    <property name="authenticationService"   ref="AuthenticationService" />
+		<property name="personService"   ref="PersonService" />
+		<property name="useHeader"	value="false" />
+	  </bean>
+	  
+	</beans>
+
+Note the `useHeader` property defaults to "false", change the value to "true" in order to enable support
+for the REMOTE_USER HTTP header.
+
+3. Save the file and restart Alfresco.
---
+++ source/java/org/alfresco/module/webscripts/sso/SSOAuthenticatorFactory.java
@@ -0,0 +1,221 @@
+/*
+ * Copyright (C) 2005-2011 Alfresco Software Limited.
+ *
+ * This file is part of Alfresco
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.alfresco.module.webscripts.sso;
+
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.extensions.webscripts.Authenticator;
+import org.springframework.extensions.webscripts.Description.RequiredAuthentication;
+import org.springframework.extensions.webscripts.servlet.WebScriptServletRequest;
+import org.springframework.extensions.webscripts.servlet.WebScriptServletResponse;
+
+import org.alfresco.repo.security.authentication.AuthenticationUtil;
+import org.alfresco.service.cmr.security.AuthenticationService;
+import org.alfresco.service.cmr.security.PersonService;
+
+import org.alfresco.repo.web.scripts.servlet.BasicHttpAuthenticatorFactory;
+
+
+/**
+ * SSO Authentication for Web Scripts (CMIS, specifically).
+ * 
+ * @author Luis Sala (luis.sala@alfresco.com)
+ * @version $Id: SSOAuthenticatorFactory.java 28112 2011-05-30 05:46:41Z lsala $
+ */
+public class SSOAuthenticatorFactory
+    extends BasicHttpAuthenticatorFactory
+{
+    // Logger
+    private final static Log log = LogFactory.getLog(SSOAuthenticatorFactory.class);
+    
+    private final static String HTTP_HEADER_NAME_USER_ID = "REMOTE_USER";
+    
+
+    // Component dependencies
+    private AuthenticationService   authenticationService;
+    private PersonService   personService;
+    private boolean useHeader;
+
+    
+    /**
+     * @param authenticationService
+     */
+    @Override
+    public void setAuthenticationService(final AuthenticationService authenticationService)
+    {
+        super.setAuthenticationService(authenticationService);
+        this.authenticationService = authenticationService;
+    }
+    
+    public void setPersonService(final PersonService personService)
+    {
+        this.personService = personService;
+    }
+    
+    public void setUseHeader(final boolean useHeader)
+    {
+    	this.useHeader = useHeader;
+    }
+
+
+    /* (non-Javadoc)
+     * @see org.alfresco.web.scripts.servlet.ServletAuthenticatorFactory#create(org.alfresco.web.scripts.servlet.WebScriptServletRequest, org.alfresco.web.scripts.servlet.WebScriptServletResponse)
+     */
+    public Authenticator create(final WebScriptServletRequest req, final WebScriptServletResponse res)
+    {
+        return new SSOAuthenticator(req, res);
+    }
+    
+    
+    /**
+     * SSO Authentication - Masquerades as the user passed via the REMOTE_USER HTTP header.
+     * 
+     * @author Luis Sala (luis.sala@alfresco.com)
+     */
+    public class SSOAuthenticator
+        extends BasicHttpAuthenticatorFactory.BasicHttpAuthenticator
+    {
+        // dependencies
+        private WebScriptServletRequest  servletReq;
+        private WebScriptServletResponse servletRes;
+        
+        
+        /**
+         * Construct
+         * 
+         * @param req
+         * @param res
+         */
+        public SSOAuthenticator (final WebScriptServletRequest req, final WebScriptServletResponse res)
+        {
+            super(req, res);
+            
+            this.servletReq = req;
+            this.servletRes = res;
+        }
+        
+    
+        /* (non-Javadoc)
+         * @see org.alfresco.web.scripts.Authenticator#authenticate(org.alfresco.web.scripts.Description.RequiredAuthentication, boolean)
+         */
+        @Override
+        public boolean authenticate(final RequiredAuthentication required, final boolean isGuest)
+        {
+            boolean             result = false;
+            HttpServletRequest  req    = servletReq.getHttpServletRequest();
+            HttpServletResponse res    = servletRes.getHttpServletResponse();
+            
+            String userId = req.getRemoteUser();
+            
+            if ((userId == null || userId.equals("")) && useHeader)
+            	userId = req.getHeader(HTTP_HEADER_NAME_USER_ID);            
+            
+            if (log.isDebugEnabled()) {
+                log.debug("Received request: " + requestToString(req));
+                log.debug("UserId Header: "+req.getHeader(HTTP_HEADER_NAME_USER_ID));
+                log.debug("UserId CGI Var: "+req.getRemoteUser());
+            }
+            
+            if (!(userId == null || userId.equals("")))
+            {
+            	if (log.isDebugEnabled())
+                    log.debug("Looking for SSO User:" + userId);
+            	
+            	// Force the authenticated user to be the one provided by SSO rather than the one in the HTTP Basic Auth credentials
+                try {
+                	
+                	// Authenticate as System User to locate account.
+                	AuthenticationUtil.setFullyAuthenticatedUser(AuthenticationUtil.getSystemUserName());
+                	
+                	if (personService.personExists(userId)) {
+                		if (log.isDebugEnabled())
+                            log.debug("User Found:" + userId);
+                		AuthenticationUtil.setFullyAuthenticatedUser(userId);
+                		result = true;
+                	} else {
+                		if (log.isDebugEnabled())
+                            log.debug("User " + userId + " Not Found. Switching to Guest Privileges");
+                		AuthenticationUtil.setFullyAuthenticatedUser(AuthenticationUtil.getGuestUserName());
+                	}
+                } catch (Exception e) {
+                	if (log.isDebugEnabled())
+                        log.debug("SSO Authentication Error: "+e.toString());
+                	AuthenticationUtil.setFullyAuthenticatedUser(AuthenticationUtil.getGuestUserName());
+                }
+            }
+            else // The user id sent from SSO isn't known to Alfresco
+            {
+                if (log.isDebugEnabled())
+                    log.debug(HTTP_HEADER_NAME_USER_ID+" " + userId + " not found. Switching to Basic Auth");
+
+                result = super.authenticate(required, isGuest);
+            }
+            
+            return result; 
+        }
+    
+    
+	    /**
+	     * Debugging method for obtaining the state of a request as a String.
+	     * 
+	     * @param request The request to retrieve the state from <i>(may be null)</i>.
+	     * @return The request state as a human-readable string value <i>(will not be null)</i>.
+	     */
+	    private String requestToString(final HttpServletRequest request)
+	    {
+	        StringBuffer result = new StringBuffer(128);
+	        
+	        if (request != null)
+	        {
+	            result.append("\n\tMethod: ");
+	            result.append(request.getMethod());
+	            result.append("\n\tURL: ");
+	            result.append(String.valueOf(request.getRequestURI()));
+	            result.append("\n\tHeaders: ");
+	            
+	            Enumeration<String> headerNames = request.getHeaderNames();
+	            
+	            while (headerNames.hasMoreElements())
+	            {
+	                String headerName  = headerNames.nextElement();
+	                String headerValue = request.getHeader(headerName);
+	
+	                result.append("\n\t\t");
+	                result.append(headerName);
+	                result.append(" : ");
+	                result.append(headerValue);
+	            }
+	        }
+	        else
+	        {
+	            result.append("(null)");
+	        }
+	        
+	        return(result.toString());
+	    }
+	    
+	}
+
+}
